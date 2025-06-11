7a73d7ace23cc83c8eb441acaf4aa3dff468f2fb
luferomar <luferomar@gmail.com>
2025-06-10 20:30:19 -0500

    Add files via upload

---
+++ Uber Didi/src/index.js
@@ -0,0 +1,12 @@
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import ClientRadar from './ClientRadar';
+import './index.css';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <ClientRadar />
+  </React.StrictMode>
+);
