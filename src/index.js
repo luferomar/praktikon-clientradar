c5834ecd4e37c376c6fddcdf13bca90451cbf084
LuisFer <luferomar@gmail.com>
2025-06-18 23:20:09 -0500

    Añado CSS Mapbox vía index.js o CDN

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
 import 'mapbox-gl/dist/mapbox-gl.css';
+import 'mapbox-gl/dist/mapbox-gl.css';  // ✅ esto aplica los estilos globalmente
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css';  // tus estilos
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
