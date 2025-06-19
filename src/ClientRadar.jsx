91fab1928db50215c59169589c7ba480976cdf82
LuisFer <luferomar@gmail.com>
2025-06-19 18:19:32 -0500

    Corrijo ClientRadar con panel Edison visible y botones

--- src/ClientRadar.jsx
+++ src/ClientRadar.jsx
@@ -1,6 +1,7 @@
-
+// src/ClientRadar.jsx
 import React, { useEffect } from 'react';
 import mapboxgl from 'mapbox-gl';
+import './index.css';
 
 mapboxgl.accessToken = process.env.REACT_APP_MAPBOX_TOKEN;
 
@@ -9,7 +10,7 @@ function ClientRadar() {
     const map = new mapboxgl.Map({
       container: 'map',
       style: 'mapbox://styles/mapbox/streets-v11',
-      center: [-74.1, 4.7], // Bogotá
+      center: [-74.1, 4.7],
       zoom: 12
     });
 
@@ -19,7 +20,19 @@ function ClientRadar() {
       .addTo(map);
   }, []);
 
-  return <div id="map" style={{ width: '100%', height: '100vh' }} />;
+  return (
+    <div className="map-container">
+      <div id="map" style={{ width: '100%', height: '100vh' }}></div>
+      <div className="panel-edison">
+        <h3>Panel Edison</h3>
+        <button onClick={() => alert('Función 1')}>Función 1</button>
+        <button onClick={() => alert('Función 2')}>Función 2</button>
+        <button onClick={() => alert('Función 3')}>Función 3</button>
+      </div>
+    </div>
+  );
 }
 
 export default ClientRadar;
+
+
