5f974900da7c85411b40bf0b232755f85d7c9d61
LuisFer <luferomar@gmail.com>
2025-06-18 22:39:48 -0500

    Corrijo render completo del radar: botones, GPS, estadísticas

--- src/index.js
+++ src/index.js
@@ -1,12 +1,1 @@
-
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import ClientRadar from './ClientRadar';
-import './index.css';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <ClientRadar />
-  </React.StrictMode>
-);
+import 'mapbox-gl/dist/mapbox-gl.css';
